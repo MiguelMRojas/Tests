9af210ca89c83f1ba37c05883638add8e62cf1cc
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-03-31 14:38:20 -0500

    Hbs - Html CREATE

--- src/server.js
+++ src/server.js
@@ -1,31 +1,36 @@
 const express = require('express');
+const { create } = require('express-handlebars');
 const path = require('path');
 
 // Initializations
 const app = express();
 
-
-
-//Settings
+// Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  ".hbs",
+  create({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout: "main",
+    extname: ".hbs",
+  }).engine
+);
+app.set("view engine", ".hbs");
 
-
-//Midlewares
+// Middlewares
 app.use(express.urlencoded({extended: false}));
 
+// Global Variables
 
-//Global Variables
-
-
-//Routes
-app.get('/', (req, res) =>{
-  res.send('Hola')
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-
-//Static Files
-app.use(express.static(path.join(__dirname + 'public')));
-
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
+
